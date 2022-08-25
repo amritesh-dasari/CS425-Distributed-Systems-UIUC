f968393679221f0249a287750d0ec9fb54426cf1
Amritesh Dasari <dmamritesh@gmail.com>
2022-08-25 11:46:13 -0500

    Testing Threads

---
+++ MP1 Logging Implementation/client.py
@@ -0,0 +1,35 @@
+# Client.py
+import time
+import os
+import socket
+import threading
+
+VMS = []
+PORT = 8080
+
+with open('serverslist.txt','r') as serverslist:
+    # Reading Servers List from the text file
+    lines = serverslist.readlines()
+    serverslist.close()
+    for line in lines:
+        serverip = line.split("\n")[0]
+        VMS.append(serverip)
+
+def querythreads(host, port, pattern):
+    # WIP testing individual threads
+    print(host, port, pattern)
+
+def search_query(pattern):
+    # Search Query method to maintain multiple Threads
+    startwatch = time.time()
+    threads=[]
+    for vm in VMS:
+        threads.append(threading.Thread(target=querythreads, args=(vm,PORT,pattern)))
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    stopwatch=time.time()
+    print("Stopwatch Time : ",startwatch-stopwatch)
+
+search_query("Bruh")
